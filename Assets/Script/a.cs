bc7b259b97c21e645ceef016b813373518a269f9
DESKTOP-5PNN0T1\vantan <kanten3005@gmail.com>
2024-05-20 12:59:06 +0900

    更新後

--- Assets/Script/a.cs
+++ Assets/Script/a.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngineInternal;
 
 public class a : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        int aaa;
+        aaa
+            = 0;
+        aaa = 0;
     }
-
+    git
     // Update is called once per frame
     void Update()
     {
